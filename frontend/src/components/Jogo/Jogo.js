7a45ccbcb44f9e4b4900048381886268203d40ac
Tatafesan <talitafesan@gmail.com>
2022-08-23 09:55:32 -0400

    Criação do estado estaPulando e detecção do clique (WIP)

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -2,8 +2,21 @@ import "./Jogo.css";
 import nuvens from "../../assets/clouds.png";
 import cano from "../../assets/pipe.png";
 import mario from "../../assets/mario.gif";
+import { useState } from "react";
 
 function Jogo() {
+  //Criamos o estado 'estaPulando' com valor padrao 'false'
+  const [estaPulando, setEstaPulando] = useState(false);
+
+  document.onkeydown = function () {
+    console.log("On Key Down");
+
+    estaPulando = true;
+  };
+
+  console.log (15, { estaPulando });
+
+  //jsx
   return (
     <div className="jogo">
       <img className="nuvens" src={nuvens} alt="Nuvens" />
